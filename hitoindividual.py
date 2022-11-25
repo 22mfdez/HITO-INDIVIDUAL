bceaed03a7572ff05639aecf344ab8d1550f93b9
22mfdez <117905017+22mfdez@users.noreply.github.com>
2022-11-26 00:00:30 +0100

    Add files via upload

--- hitoindividual.py
+++ hitoindividual.py
@@ -1,12 +1,18 @@
-class Interface():
-    def menu():
+import sys
+ 
+class Interface:
+    def __init__(self) -> None:
         pass
-    def mostrarDatos():
+    def menu(self):
         pass
-    def pago():
+    def mostrarDatos(self):
         pass
-
+    def pago(self):
+        pass
+ 
 class Cliente(Interface):
+    
+  
     def __init__(self):
         super().__init__()
         print('-----BIENVENIDO/A. PROCESO DE REGISTRO EN CARGA-----')
@@ -14,81 +20,90 @@ class Cliente(Interface):
         self.nombre=input('Introduzca su nombre: ')
         self.apellidos=input('Introduzca sus apellidos: ')
         self.direccion=input('Introduzca su dirección: ')
-        self.telefono=int(input('Introduzca su número de teléfono: '))
+        self.telefono=input('Introduzca su número de teléfono: ')
         self.correo=input('Introduzca su correo eléctronico: ')
-        self.mostrarDatos()
-        self.Menu()
-
-    def mostrarDatos():
-        print('Se ha registrado correctamente.')
-
-
-    def Menu(self):
-        opcion=0
-        while opcion!=0:
-            print("1- Selección de productos")
-            print("2- Método de pago")
-            print("3- Seguimiento del pedido")
-            opcion=int(input("Ingrese su opcion: "))
-            if opcion==1:
-                print('Ha seleccionado Selección de productos')
-                self.productos()
-            elif opcion==2:
-                print('Ha seleccionado Método de Pago')
-                self.pago()
-            elif opcion==3:
-                print('Ha seleccionado Seguimiento del pedido')
-                self.seguimiento()
-            else:
-                print('Por favor, elija una opción del menú')
-                self.Menu()
-
-    #def productos(self, nombre,  precio):
-        #self.nombre=nombre
-        #self.precio=precio
-    #productos=[]
-    #productos.append()
+        print('---------- SE HA REGISTRADO CORRECTAMENTE --------')
+        print('--------- LOADING MENU 98% ---------')
+    
+   
+    def menu(self):
+        print("Bienvenido/a a la Selección de productos. Por favor, siga las instrucciones del menú")
+        self.productos()
 
 
-    def pago(self):
-        opcion=0
-        print('----Procesando su pago. Por favor, seleccione País en el que se encuentra---')
-        print('1--España')
-        print('2--Fuera de España')
-        opcion=int(input('Seleccione una opción: '))
-        while opcion !=0:
-            if opcion==1:
-                print('¡Recuerda! Se implementa un 1.15 de IVA ')
-                preciofinal= (PRODUCTO SELECIONADO +1.15)
-                print(f'El precio total será de ------- euros')
-            elif opcion==2:
-                print('¡Recuerda! Para envíos fuera de España se implementa un 1.30 de IVA')
-                preciofinal= (PRODUCTO SELECCIONADO + 1.30)
-                print(f'El precio total será de ----- euros')
-            else:
-                print('Selección errónea. Por favor, seleccione de nuevo')
-                self.pago()
-        print('-------Método de Pago-----')
-        opcion1=0
-        print('1--Tarjeta de crédito')
-        print('2--Contra-Reembolso')
-        opcion1=int(input('Seleccione método de pago: '))
-        while opcion1!=0:
-            if opcion1==1:
-                print('Pago completado correcto')
-                self.seguimiento()
-            elif opcion1==2:
-                print('Opción de Contra-Reembolso efectuada correctamente')
-                self.seguimiento()
-            else:
-                print('Error de método de pago. Por favor, vuelva a seleccionar')
-                return
-
-    def seguimiento(self):    
-        print('GRACIAS POR SU COMPRA.')
-        print('Se ha enviado un SMS al teléfono registrado')
-        print('Se ha enviado un correo con un número de seguimiento')
+    def productos(self):
+        listaproductos=['camiseta levis', 'pantalon vaquero diesel', 'pack gorros', 'jersey rayas', 'zapatillas nike', 'zapatillas reebook', 'bolso pedreria', 'pack bisuteria']
+        precios=[14.18, 60.99, 13.99, 14.99, 70.75, 65.95, 39.99, 16.99]
+        carrito=[] 
+        while True:
+            print('--------Procesando selección de productos. Por favor, seleccione una opción')
+            print('1- Añadir productos que desee comprar')
+            print('2- Pago de productos')
+            opcion=int(input('Seleccione opción del menú anterior 1/2: '))
         
+
+            if opcion==1:
+                print(listaproductos)
+                añadirproducto=input('Producto que desea añadir al carrito: ').lower()
+                print(f' el producto {añadirproducto} se ha añadido correctamente.')
+                if añadirproducto in listaproductos:
+                    lista=listaproductos.index(añadirproducto)
+                    precio=precios[lista]
+                    print(f' El producto tiene valor de {precio} €')
+                    carrito.append(precio)
+                    preciosiniva=sum(carrito)
+                    print(f' el precio total es {preciosiniva} €')
+                
+                    
+                else:
+                    print('Lo siento. Ese producto no está disponible')
+                    
+
+            elif opcion==2:
+                preciosiniva=sum(carrito)
+                print(preciosiniva)
+                print('----Procesando su pago. Por favor, seleccione País en el que se encuentra---')
+                print('1--España')
+                print('2--Fuera de España')
+                opcion=int(input('Seleccione una opción: '))
+                if opcion==1:
+                    print('¡Recuerda! Se implementa un 1.15 de IVA ')
+                    preciofinal=(preciosiniva*1.15)
+                    preciofinal=round(preciofinal, 2)
+                    print(f'El precio total será de {preciofinal} euros')
+                elif opcion==2:
+                    print('¡Recuerda! Para envíos fuera de España se implementa un 1.30 de IVA')
+                    preciofinal= (preciosiniva*1.30)
+                    preciofinal=round(preciofinal, 2)
+                    print(f'El precio total será de {preciofinal} euros')
+                else:
+                    print('Selección errónea. Por favor, seleccione de nuevo')
+                
+                print('-------Método de Pago-----')
+                print('1--Tarjeta de crédito')
+                print('2--Contra-Reembolso')
+                opcion1=int(input('Seleccione método de pago: '))
+                if opcion1==1:
+                    print('Pago completado correcto')
+                elif opcion1==2:
+                    print('Opción de Contra-Reembolso efectuada correctamente')
+                else:
+                    print('Error de método de pago. Por favor, vuelva a seleccionar')
+            
+                print('GRACIAS POR SU COMPRA.')
+                print(f'Se ha enviado un PDF con la factura de la compra al correo {self.correo}')
+                print(f'Se ha enviado un correo al email {self.correo} y un SMS al telefono {self.telefono} con un número de seguimiento')
+                sys.exit()  #EXPLICAR POR QUE HEMOS IMPORTADOD ESTE MODULO
+           
+
+ 
+ 
+cliente=Cliente()
+cliente.menu()
+cliente.productos()
+
+ 
+
 
 
 
